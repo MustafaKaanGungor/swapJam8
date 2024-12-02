191f4de7fab155bc458a0119deab3a3870310a43
YavuzKibiroglu <24181616021@gazi.edu.tr>
2024-12-02 16:07:42 +0300

    Fixed Background Animation

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System;
 using System.Collections.Generic;
 
 public class AudioManager:MonoBehaviour
 {
     public static AudioManager instance;
+    private Scene currentScene;
+    private Scene MainMenu;
+    [SerializeField] private float slowedTime;
+
     
     void Awake()
     {
@@ -15,6 +20,19 @@ public class AudioManager:MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        MainMenu = SceneManager.GetSceneByBuildIndex(0);
+        if(currentScene == MainMenu)
+        {
+            Time.timeScale = slowedTime;
+        }
+        else
+        {
+            Time.timeScale = 1;
         }
     }
 
